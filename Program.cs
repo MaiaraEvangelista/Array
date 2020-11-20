a9959e8193cd2f302ff61bb0426b93e148340b34
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-20 11:54:56 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] listaCompras = {"maçã", "batata", "cenoura", "laranja", "mexirica"};
+            double[] precos = {2.5, 4.3, 2.8};
+
+            string[] mercados = new string[5];
+
+          //  Console.WriteLine(listaCompras[1]); 
+
+            Console.WriteLine("Trocar maçã por qual item?");
+            
+            string item = Console.ReadLine();
+
+            listaCompras[0] = Console.ReadLine();
+
+          //  Console.WriteLine(listaCompras[0]);
+
+            for(int i = 0; i < listaCompras.Length; i++){
+                Console.WriteLine(listaCompras[i]);
+            }
+
+           
+        }
+    }
+}
